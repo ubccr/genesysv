9d7004359b08f98c06e51430038d77410e9ab7cb
Mohammad Zia <mkzia@gdwdev.ccr.buffalo.edu>
2016-12-16 10:20:19 -0500

    Add gzip and minify

--- gdw/settings.py
+++ gdw/settings.py
@@ -51,6 +51,9 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'django.middleware.gzip.GZipMiddleware',
+    'htmlmin.middleware.HtmlMinifyMiddleware',
+    'htmlmin.middleware.MarkRequestMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
