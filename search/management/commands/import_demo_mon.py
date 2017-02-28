3bf1159be023b186e0184b98364be628a3a3995f
Mohammad Zia <mkzia@gdwdev.ccr.buffalo.edu>
2017-02-28 12:10:44 -0500

    Split ES settings and ES demo import

--- search/management/commands/import_demo_mon.py
+++ search/management/commands/import_demo_mon.py
@@ -1,25 +1,5 @@
 from django.core.management.base import BaseCommand
 from search.models import *
-from pybamview.models import SampleBamInfo
-
-
-FORM_TYPES = ("CharField", "ChoiceField", "MultipleChoiceField")
-
-WIDGET_TYPES = ("TextInput", "Select", "SelectMultiple", "Textarea", "UploadField")
-
-ES_FILTER_TYPES = ( "filter_term",
-                    "filter_terms",
-                    "nested_filter_term",
-                    "nested_filter_terms",
-                    "filter_range_gte",
-                    "filter_range_gt",
-                    "filter_range_lte",
-                    "filter_range_lt",
-                    "nested_filter_range_gte",
-                    "filter_exists",
-                    "must_not_exists",
-                    "nested_filter_exists",
-)
 
 EYE_COLOR_CHOICES = ( "blue", "brown", "green")
 
@@ -67,15 +47,6 @@ class Command(BaseCommand):
 
 
         SearchOptions.objects.get_or_create(dataset=dataset_object)
-
-        for name in FORM_TYPES:
-            FormType.objects.get_or_create(name=name)
-
-        for name in WIDGET_TYPES:
-            WidgetType.objects.get_or_create(name=name)
-
-        for name in ES_FILTER_TYPES:
-            ESFilterType.objects.get_or_create(name=name)
 
         dataset_object = Dataset.objects.get(name='demo_dataset')
 
---
+++ search/management/commands/import_es_settings.py
@@ -0,0 +1,34 @@
+from django.core.management.base import BaseCommand
+from search.models import *
+
+FORM_TYPES = ("CharField", "ChoiceField", "MultipleChoiceField")
+
+WIDGET_TYPES = ("TextInput", "Select", "SelectMultiple", "Textarea", "UploadField")
+
+ES_FILTER_TYPES = ( "filter_term",
+                    "filter_terms",
+                    "nested_filter_term",
+                    "nested_filter_terms",
+                    "filter_range_gte",
+                    "filter_range_gt",
+                    "filter_range_lte",
+                    "filter_range_lt",
+                    "nested_filter_range_gte",
+                    "filter_exists",
+                    "must_not_exists",
+                    "nested_filter_exists",
+)
+
+
+class Command(BaseCommand):
+
+    def handle(self, *args, **options):
+
+        for name in FORM_TYPES:
+            FormType.objects.get_or_create(name=name)
+
+        for name in WIDGET_TYPES:
+            WidgetType.objects.get_or_create(name=name)
+
+        for name in ES_FILTER_TYPES:
+            ESFilterType.objects.get_or_create(name=name)
