e54a32b35bfb864661f7ddcdb6ce77af17dd895b
Mohammad K. Zia <mkzia@buffalo.edu>
2017-06-02 11:16:53 -0400

    Update import rs id to add tqdm

--- msea/management/commands/import_rs_id_genes_variant_set.py
+++ msea/management/commands/import_rs_id_genes_variant_set.py
@@ -1,6 +1,9 @@
 from django.db import transaction
-
-
+from tqdm import tqdm
+from collections import deque
+import sys
+import os
+import statistics
 from django.core.management.base import BaseCommand
 from msea.models import *
 
@@ -17,6 +20,31 @@ VARIANT_FILE_TYPE_CHOICES = (('ass', 'All Silent SNVs'),
                          ('revelset', 'REVEL score >= 0.50'),
                     )
 
+def estimate_no_lines(filename, no_lines_for_estimating):
+    no_lines = 0
+    size_list = deque()
+
+    with open(filename, 'r') as fp:
+        for line in fp:
+            if line.startswith('#'):
+                continue
+
+            if no_lines_for_estimating < no_lines:
+                break
+
+            size_list.append(sys.getsizeof(line))
+
+            no_lines += 1
+
+    filesize = os.path.getsize(filename)
+
+    no_lines = int(filesize/statistics.median(size_list))
+
+    return no_lines
+
+
+
+
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
@@ -27,38 +55,61 @@ class Command(BaseCommand):
 
 
         DATASETS = (
-                ("sim_wgs", "SIM Sensitive Whole Genome Sequence", "20170222_SIM_WGS-20170416-expand_lookahead.txt"),
-                ("sim_res", "SIM Resistant Whole Exome Sequence", "20170307_SIM_Control-20170416-expand_lookahead.txt"),
+                ("sim_wgs",
+                 "SIM Sensitive Whole Genome Sequence",
+                 "msea",
+                 "msea",
+                 "199.109.195.45",
+                 "9200",
+                 "20170222_SIM_WGS-20170416-expand_lookahead.txt"),
+
+
+                ("sim_res",
+                 "SIM Resistant Whole Exome Sequence",
+                 "msea",
+                 "msea",
+                 "199.109.195.45",
+                 "9200",
+                 "20170307_SIM_Control-20170416-expand_lookahead.txt"),
         )
 
-        for dataset, display_name, filename in DATASETS:
+
+
+        for dataset, display_name, es_index_name, es_type_name, es_host, es_port, filename in DATASETS:
             msea_datset_obj, _ = MseaDataset.objects.get_or_create(dataset=dataset,
                                                               display_name=display_name,
+                                                              es_index_name=es_index_name,
+                                                              es_type_name=es_type_name,
+                                                              es_host=es_host,
+                                                              es_port=es_port,
                                                               is_public=True)
 
-            with transaction.atomic():
-                with open('./msea/management/commands/data/%s' %(filename), 'r') as fp:
-                    for idx, line in enumerate(fp):
-                        if line.startswith('#') or not line.strip():
-                            continue
 
-                        # print(line)
-                        line_split = line.strip().split()
-                        gene_name = line_split[0].strip()
-                        rs_id = line_split[1].strip()
-                        variants = line_split[2:]
-                        if 1:#(idx % 1000) == 0:
-                            print(idx, line_split)
-                        gene_object, _ = Gene.objects.get_or_create(gene_name=gene_name)
+            filename_path = './msea/management/commands/data/%s' %(filename)
+            no_lines = estimate_no_lines(filename_path, 200000)
+            with open(filename_path, 'r') as fp:
+                for line in tqdm(fp, total=no_lines):
+                #for idx, line in enumerate(fp):
+                    if line.startswith('#') or not line.strip():
+                        continue
+
+                    # print(line)
+                    line_split = line.strip().split()
+                    gene_name = line_split[0].strip()
+                    rs_id = line_split[1].strip()
+                    variants = line_split[2:]
+                    # if (idx % 1000) == 0:
+                    #     print(idx, line_split)
+                    gene_object, _ = Gene.objects.get_or_create(gene_name=gene_name)
 
 
-                        rs_object, _ = ReferenceSequence.objects.get_or_create(msea_dataset=msea_datset_obj,
-                                                                               rs_id=rs_id,
-                                                                               gene=gene_object)
-                        for variant in variants:
-                            var_obj = VariantSet.objects.get(short_name=variant.strip())
-                            rs_object.variants.add(var_obj)
+                    rs_object, _ = ReferenceSequence.objects.get_or_create(msea_dataset=msea_datset_obj,
+                                                                           rs_id=rs_id,
+                                                                           gene=gene_object)
+                    for variant in variants:
+                        var_obj = VariantSet.objects.get(short_name=variant.strip())
+                        rs_object.variants.add(var_obj)
 
-                        rs_object.save()
+                    rs_object.save()
 
 
