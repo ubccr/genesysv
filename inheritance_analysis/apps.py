af861c5266e03ab69ce1cf01adbc32eb3a604fb3
Mohammad K. Zia <mkzia@buffalo.edu>
2017-11-14 14:59:53 -0500

    Finish refactoring Trio to Inheritance analysis.

---
+++ inheritance_analysis/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class InheritanceAnalysisConfig(AppConfig):
+    name = 'inheritance_analysis'
---
+++ inheritance_analysis/urls.py
@@ -0,0 +1,8 @@
+from django.conf.urls import include, url
+from .views import *
+
+urlpatterns = [
+    url(r'^$', InheritanceAnalysisRequestListView.as_view(), name='inheritance-analysis-request-list'),
+    url(r'^create/$', inheritance_analysis_request_create, name='inheritance-analysis-request-create'),
+    url(r'^delete/(?P<pk>[0-9]+)/$', InheritanceAnalysisRequestDelete.as_view(), name='inheritance-analysis-request-delete'),
+]
